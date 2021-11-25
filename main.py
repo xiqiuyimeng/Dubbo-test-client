3158a356e64f9d727e327f3e9d49e420058a7188
xiqiuyimeng <847466569@qq.com>
2021-11-25 13:37:13 +0800

    增加图标，设置任务栏图标

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import sys
 
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIcon
+
 from static import image_rc
 from src.ui.main_window import MainWindow
 _author_ = 'luwt'
@@ -10,6 +12,8 @@ _date_ = '2021/10/31 17:49'
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
+    # 设置任务栏图标
+    app.setWindowIcon(QIcon(":/icon/exec.png"))
     # 获取当前屏幕分辨率
     desktop = QtWidgets.QApplication.desktop()
     screen_rect = desktop.screenGeometry()
